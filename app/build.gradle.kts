c4f6f63d34d65dfb0a957f7387e5e7cdfc6a908a
sarsarahdzhu <zhud0228@terpmail.umd.edu>
2025-04-21 20:23:34 -0400

    added google ads dependency and xml files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-ads:24.1.0" )
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
