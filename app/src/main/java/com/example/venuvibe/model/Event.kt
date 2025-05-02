dc7fb49560bab6ab875c5dc582b480aa7f477809
sarsarahdzhu <zhud0228@terpmail.umd.edu>
2025-05-02 16:03:53 -0400

    added activities

--- app/src/main/java/com/example/venuvibe/model/Event.kt
+++ app/src/main/java/com/example/venuvibe/model/Event.kt
@@ -1,5 +1,7 @@
 package com.example.venuvibe.model
 
+import java.io.Serializable
+
 // This class is for representing one event
 data class Event(
     var id: String        = "",
@@ -9,4 +11,10 @@ data class Event(
     var longitude: Double = 0.0,
     var date: Long        = 0L,
     var averageRating: Float = 0f
-)
+) : Serializable {
+
+fun getDateFormatted(): String {
+    val formatter = java.text.SimpleDateFormat("MM/dd/yy, h:mm a", java.util.Locale.getDefault())
+    return formatter.format(java.util.Date(date))
+}
+}
